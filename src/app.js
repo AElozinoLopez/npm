1a1f84d05a4110ddbbbd74a4be10ed686c3f5f8e
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-03-25 16:58:28 +0100

    required the moreHelpers funtions in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 // console.log("Hello world");
 
 const helpers = require('./helpers');
+const moreHelpers = require('./moreHelpers');
 
-console.log(helpers);
-console.log(helpers.name);
-console.log(helpers.sayHi());
+// console.log(helpers);
+// console.log(helpers.name);
+// console.log(helpers.sayHi());
 
-const {name, sayHi()} = require('./helpers');
-console.log(name);  // Elozino
-console.log(sayHi); // Hi undefined
+// const {name, sayHi()} = require('./helpers');
+// console.log(name);  // Elozino
+// console.log(sayHi); // Hi undefined
+
+console.log(moreHelpers.sayHello());
+console.log(moreHelpers.sayGoodbye());
+console.log(moreHelpers.firstName);
