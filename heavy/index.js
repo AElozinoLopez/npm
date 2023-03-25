3984908be00f3a4deae0bd34849bcb71a0cb1564
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-03-25 18:12:47 +0100

    Created an index.js file, imported all files and exported it for use globally

---
+++ heavy/index.js
@@ -0,0 +1,12 @@
+const file1 = require('./file1');
+const file2 = require('./file2');
+const file3 = require('./file3');
+const file4 = require('./file4');
+
+module.exports = {
+    file1,
+    file2,
+    file3,
+    file4
+}
+
