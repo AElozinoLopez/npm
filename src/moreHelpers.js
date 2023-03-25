a6fd4b8ac3338fb51d5eaad15f25980c6e54d01c
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-03-25 16:52:54 +0100

    Created the moreHelpers.js file and used the exports keyword for export

---
+++ src/moreHelpers.js
@@ -0,0 +1,9 @@
+// Here we are exporting using the exports keyword only instead of using the module.exports object.
+
+exports.sayHello = function() {
+    console.log("Hello!");
+  };
+exports.sayGoodbye = function() {
+    console.log("Goodbye!");
+  };
+exports.firstName = "Elie";
